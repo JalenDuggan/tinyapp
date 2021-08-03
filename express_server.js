5bf73ab2d32a97388b92abaca5f0a278ad17a267
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-08-03 12:55:42 +0000

    Added a POST Route to Receive the Form Submission and a function that generates a random string, checks to see if it exist if the database. If is it call itself and if not it returns the string

--- express_server.js
+++ express_server.js
@@ -4,6 +4,23 @@ const PORT = 8080; //default port 8080
 
 app.set("view engine", "ejs"); //tells the Express app to use EJS as its templating engine
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateRandomString = () => {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let result = '';
+  for (var i = 6; i > 0; --i) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  if (urlDatabase[result]) {
+    return generateRandomString()
+  } else {
+    return result;
+  }
+  
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -26,11 +43,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.params);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body)
+  res.send("Ok");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
